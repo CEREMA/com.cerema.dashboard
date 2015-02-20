fe1bd6c45b230549725890e9c944993d798f7a6b
Stéphane Zucatti <stephane.zucatti@me.com>
2015-02-20 16:01:24 +0100

    Update# 110

--- src/Contents/Application/app/view/main/VFacture.js
+++ src/Contents/Application/app/view/main/VFacture.js
@@ -154,7 +154,7 @@ App.view.define('main.VFacture',
 		width: "100%"
 	},
 	{
-		fieldLabel: 'Marché',
+		fieldLabel: 'Fournisseur',
 		name: 'marche',
 		allowBlank: false,
 		hiddenName: 'cbo_marche',
