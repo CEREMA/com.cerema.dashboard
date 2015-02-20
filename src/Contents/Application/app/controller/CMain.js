3ad01c8dcc994cb841915af477899ee17b5d7f3a
Stéphane Zucatti <stephane.zucatti@me.com>
2015-02-20 14:29:04 +0100

    Update# 61

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -43,7 +43,7 @@ App.controller.define('CMain', {
 			},
 			"button": {
 				toggle: function(p,press) {
-					alert('x');
+					/*alert('x');*/
 					App.get('button').toggle(false);
 					p.toggle(press);
 				}
