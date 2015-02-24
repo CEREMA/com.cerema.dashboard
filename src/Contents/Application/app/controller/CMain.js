28a0fccc1a8badc4c971d9d4dd5cceda28a244e4
Stéphane Zucatti <stephane.zucatti@me.com>
2015-02-24 09:20:48 +0100

    Update# 150

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -82,6 +82,8 @@ App.controller.define('CMain', {
 		App.get('combo#cbo_marche').getStore().getProxy().extraParams.cat=cat;
 		App.get('combo#cbo_marche').getStore().load();
 		if (p.facture) {
+			var color=p.facture.etiquette;
+			if (color=="FF9900") App.get('button#borange').toggle(true);
 			App.get('textfield#ej').setValue(p.facture.ej);
 			App.get('textfield#prestation').setValue(p.facture.prestation);
 		};
