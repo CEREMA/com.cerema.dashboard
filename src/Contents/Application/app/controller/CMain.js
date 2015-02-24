f35645005fdacf741d6c09be394aac729b05ba19
Stéphane Zucatti <stephane.zucatti@me.com>
2015-02-24 11:32:45 +0100

    Update# 155

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -92,6 +92,8 @@ App.controller.define('CMain', {
 			App.get('textfield#prestation').setValue(p.facture.prestation);
 			App.get('numberfield#montant_prev').setValue(p.facture.montant_prev);
 			App.get('numberfield#montant_facture').setValue(p.facture.montant_facture);
+			App.get('textfield#reference').setValue(p.facture.reference);
+			App.get('combo#gim').setValue(p.facture.marche);
 		};
 		console.log(p.facture);
 	},
