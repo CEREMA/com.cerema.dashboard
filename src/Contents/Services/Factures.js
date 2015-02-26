d1d505559b9ddd609f43699e5253fe15d445e9da
Stéphane Zucatti <stephane.zucatti@me.com>
2015-02-26 14:53:14 +0100

    Update# 204

--- src/Contents/Services/Factures.js
+++ src/Contents/Services/Factures.js
@@ -9,6 +9,18 @@ Factures = {
 	get: function(o,cb) {
 		var db=Factures.using('db');		
 		db.model('dashboard',db.sql('factures_get',{ID: o.id}),cb);
+	},
+	insert: function(o,cb) {
+	
+	},
+	update: function(o,cb) {
+		Factures.using('db').post('dashboard','factures',o,function(r){
+			if (o._BLOB) {
+				AO.upload_blob(o._BLOB,0,function() {
+					cb(r);
+				});
+			} else cb(r);
+		});	
 	}
 };
 
