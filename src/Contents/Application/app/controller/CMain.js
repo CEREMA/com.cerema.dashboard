353ea4427c8f39fd6c7cd875b1b080c04a69d0e0
Stéphane Zucatti <stephane.zucatti@me.com>
2015-02-26 14:23:41 +0100

    Update# 199

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -157,7 +157,7 @@ App.controller.define('CMain', {
 			App.Docs.get(p.facture.DOC,function(err,row){
 				App.get('uploadfilemanager#up').setFiles(row.result.data);
 			});		*/
-		var data=[
+		var data={
 			/*prestation:,
 			reference:,
 			gim:,*/
@@ -173,7 +173,7 @@ App.controller.define('CMain', {
 			date_servicefait:,
 			commentaire:,
 			DOC: ''*/
-		];
+		};
 		/*if (color=="FF9900") App.get('button#borange').toggle(true);
 			if (color=="000000") App.get('button#bblack').toggle(true);
 			if (color=="FF0000") App.get('button#bred').toggle(true);
