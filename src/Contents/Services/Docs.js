3d42ae991544ec4a549f3996365fc6fdf4082f05
Stéphane Zucatti <stephane.zucatti@me.com>
2015-02-26 10:32:43 +0100

    Update# 183

--- src/Contents/Services/Docs.js
+++ src/Contents/Services/Docs.js
@@ -1,6 +1,6 @@
 Docs = {
 	get: function(o,cb) {
-		Docs.using('db').model('dashboard','select docId tmpfilename,filename,type filetype,size filesize from docs where docId="'+o+'"',cb);
+		Docs.using('db').model('dashboard','select docId,filename,type filetype,size filesize from docs where docId="'+o+'"',cb);
 	}
 };
 
