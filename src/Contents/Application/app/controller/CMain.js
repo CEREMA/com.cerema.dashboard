8d5278fd08af458750be883dd8ddba4eff8d217f
Stéphane Zucatti <stephane.zucatti@me.com>
2015-02-26 09:46:14 +0100

    Update# 172

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -85,6 +85,7 @@ App.controller.define('CMain', {
 	},
 	facture_onShow: function(p)
 	{	
+		App.view.create('Ext.ux.window.VisualSQLQueryBuilder').show();
 		var cat=App.get('grid#MainGrid').getStore().getProxy().extraParams.id;
 		App.get('combo#cbo_marche').getStore().getProxy().extraParams.cat=cat;
 		App.get('combo#cbo_marche').getStore().load();
