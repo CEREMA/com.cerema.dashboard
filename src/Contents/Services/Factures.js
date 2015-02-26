23b7134df222ab57e57ba105d2c8f58377bae5cf
Stéphane Zucatti <stephane.zucatti@me.com>
2015-02-26 15:02:04 +0100

    Update# 205

--- src/Contents/Services/Factures.js
+++ src/Contents/Services/Factures.js
@@ -10,13 +10,28 @@ Factures = {
 		var db=Factures.using('db');		
 		db.model('dashboard',db.sql('factures_get',{ID: o.id}),cb);
 	},
+	upload_blob: function(list,ndx,cb)
+	{
+		if (!list[ndx]) {cb();return;}
+		Factures.using('db').query('dashboard','insert into docs VALUES ("'+list[ndx].tmpfilename+'","-1","-1","-1","-1")',function() {
+			Factures.using('db').post('dashboard','docs',{
+				docId: list[ndx].docId,
+				_blob: App.upload.toBase64(list[ndx].tmpfilename),
+				filename: list[ndx].filename,
+				type: list[ndx].filetype,
+				size: list[ndx].filesize
+			},function() {
+				Factures.upload_blob(list,ndx+1,cb);
+			});
+		});
+	},	
 	insert: function(o,cb) {
 	
 	},
 	update: function(o,cb) {
 		Factures.using('db').post('dashboard','factures',o,function(r){
 			if (o._BLOB) {
-				AO.upload_blob(o._BLOB,0,function() {
+				Factures.upload_blob(o._BLOB,0,function() {
 					cb(r);
 				});
 			} else cb(r);
