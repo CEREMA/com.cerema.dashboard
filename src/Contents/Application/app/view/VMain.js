9bba02d872b93e6220e928b5198b85a0ce8ca3f8
Stephane Zucatti <stephane.zucatti@me.com>
2015-03-06 13:49:44 +0100

    Update# 328

--- src/Contents/Application/app/view/VMain.js
+++ src/Contents/Application/app/view/VMain.js
@@ -128,9 +128,8 @@ App.view.define('VMain', {
 				{header: "Echéance", width: 80, sortable: true, renderer: Ext.util.Format.dateRenderer('Y-m-d'), dataIndex: 'echeance'},				
 				{header: "Mt prév. <small>HT</small>", width: 80, sortable: true, align:"right", renderer:  Ext.util.Format.numberRenderer('0.00'), dataIndex: 'montant_prev',summaryType: 'sum'},
 				{header: "Mt prév. <small>TTC</small>", width: 80, sortable: true, align:"right", renderer:  function(v,r){
-					console.log(r);
 					var total=App.get('textfield#totalprevttc').getValue()*1;
-					total+=v*1.2;
+					if (r.recordIndex!=-1) total+=v*1.2;
 					App.get('textfield#totalprevttc').setValue(total);
 					alert(total);
 					return '<div style="color: blue">'+Ext.util.Format.number(v*1.2, '0.00')+'</div>';
