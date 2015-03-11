8e9d962df48cc8c750d33b90e34c817059acbf6d
Stephane Zucatti <stephane.zucatti@me.com>
2015-03-11 09:43:29 +0100

    Update# 493

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -23,9 +23,7 @@ App.controller.define('CMain', {
 			},
 			"grid#MainGrid checkcolumn": {
 				checkchange: function(column, rowIdx, checked, eOpts){
-					var id_facture=App.get("grid#MainGrid").getStore().getAt(rowIdx).data.id_facture;
-					console.log(App.get("grid#MainGrid").getStore().getAt(rowIdx).data);
-					return;
+					var id_facture=App.get("grid#MainGrid").getStore().getAt(rowIdx).data.idfacture;
 					if (checked) var checked=1; else var checked=false;
 					var o={
 						id: id_facture,
