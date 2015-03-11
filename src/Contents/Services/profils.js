35a6cceb236ff41b5ab092419676eaee86a7514c
Stephane Zucatti <stephane.zucatti@me.com>
2015-03-11 11:00:15 +0100

    Update# 511

--- src/Contents/Services/profils.js
+++ src/Contents/Services/profils.js
@@ -2,6 +2,7 @@ profils= {
 	get: function(o,cb)
 	{
 		var db=profils.using('db');
+		console.log('select * from profils where profil_code='+o);
 		db.query('dashboard','select * from profils where profil_code='+o,cb);
 	}
 };
