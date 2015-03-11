d39f9f1b9509b8c1a6f08d69a9082ccd5aa99659
Stephane Zucatti <stephane.zucatti@me.com>
2015-03-11 11:02:30 +0100

    Update# 513

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -328,11 +328,10 @@ App.controller.define('CMain', {
 	{
 		Auth.login(function(user) {
 			App.profils.get(user.uid,function(r,x) {
-				console.log(r);
-				console.log(x);
 				if (r.length==0) {					
 					App.disabled();
-					//alert("Vous n'avez pas accès à l'application.");
+				} else {
+					console.log(Auth.User);
 				}
 			});
 		});
