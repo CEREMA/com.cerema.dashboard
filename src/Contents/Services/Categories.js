c622975d24ea4f0570f46e3dc37bbc438ff2004f
Stephane Zucatti <stephane.zucatti@me.com>
2015-03-11 18:57:55 +0100

    Update# 534

--- src/Contents/Services/Categories.js
+++ src/Contents/Services/Categories.js
@@ -7,6 +7,7 @@
 
 Categories = {
 	getAll: function(o,cb) {
+		console.log('SELECT id,libelle FROM categories where YEAR like "%'+o.year+'%"');
 		Categories.using('db').model('dashboard','SELECT id,libelle FROM categories where YEAR like "%'+o.year+'%"',cb);
 	}
 };
