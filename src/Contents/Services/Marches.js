f09961766f130263812f1be54409d5018166f2e0
Stephane Zucatti <stephane.zucatti@me.com>
2015-03-12 09:44:40 +0100

    Update# 582

--- src/Contents/Services/Marches.js
+++ src/Contents/Services/Marches.js
@@ -8,8 +8,7 @@
 Marches = {
 	getAll: function(o,cb) {
 		var db=Marches.using('db');	
-		console.log('SELECT ID,TITLE FROM business WHERE YEAR='+o.year+' and CAT_ID='+o.cat);
-		db.model('dashboard','SELECT ID,TITLE FROM business WHERE CAT_ID='+o.cat+' and YEAR='+o.year,cb);
+		db.model('dashboard','SELECT * FROM business WHERE CAT_ID='+o.cat+' and YEAR='+o.year,cb);
 	},
 	update: function(o,cb) {
 		var db=Marches.using('db');
