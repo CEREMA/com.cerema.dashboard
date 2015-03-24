c993182222fc10ea190c7b4b8d680f1610646aee
Stephane Zucatti <stephane.zucatti@me.com>
2015-03-24 13:34:29 +0100

    Update# 681

--- src/Contents/Services/sql/filtre_getAll.sql
+++ src/Contents/Services/sql/filtre_getAll.sql
@@ -4,5 +4,4 @@ SELECT 	categorie,
 		libelle_nature,
 		annee
 		FROM    filtre
-		JOIN 	infocentre2015.nature on nature = ID_nature
 		WHERE	annee = {AN}
--- src/Contents/Services/sql/filtre_getByCat.sql
+++ src/Contents/Services/sql/filtre_getByCat.sql
@@ -3,5 +3,4 @@ SELECT 	categorie,
 		coche,
 		libelle_nature
 		FROM    filtre
-		JOIN 	infocentre2015.nature on nature = ID_nature
 		WHERE 	categorie = {ID}
