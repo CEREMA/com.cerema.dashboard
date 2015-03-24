80cc52f8cd71e09e990755f969cb5a2e1e8f76c2
Stephane Zucatti <stephane.zucatti@me.com>
2015-03-24 13:26:41 +0100

    Update# 679

--- src/Contents/Services/Infocentre.js
+++ src/Contents/Services/Infocentre.js
@@ -43,7 +43,7 @@ Infocentre = {
 							result.metaData.fields[result.metaData.fields.length]={
 								name: "NomPre",
 								type: "string",
-								length: "250",
+								length: "255",
 							};
 							cb(err,result);
 						});
@@ -55,7 +55,42 @@ Infocentre = {
 	// --------------------------------------------------------------------
 	getBaseFact: function(o, cb) {
 		var db=Infocentre.using('db');		
-		db.model('infocentre2015', db.sql('infocentre_getBaseFact',{ID: o.ID}), cb);
+		db.model('infocentre2015', db.sql('infocentre_getBaseFact',{ID: o.ID}), function(err,result) {
+			var AGENTS=[];
+			var DEPARTEMENTS=[];
+			var SERVICES=[];
+			db.model('bpclight',"select kage, concat(Nom,' ',Prenom) NomPre from agents",function(err,agents) {
+				db.model('bpclight',"select kuni,libuni from unites",function(err,departements) {
+					db.model('bpclight',"select ksub,libsub from subddis",function(err,services) {
+						AGENTS=boucle(agents,'kage','NomPre');
+						DEPARTEMENTS=boucle(departements,'kuni','libuni');
+						SERVICES=boucle(services,'ksub','libsub');
+						for (var i=0;i<result.length;i++) {
+							var rr=result[i];
+							rr.NomPre=AGENTS[rr.agent_beneficiaire];
+							rr.LibSub=SERVICES[rr.service];
+							rr.LibUni=DEPARTEMENTS[rr.departement];
+						};
+						result.metaData.fields[result.metaData.fields.length]={
+							name: "LibUni",
+							type: "string",
+							length: "255",
+						};
+						result.metaData.fields[result.metaData.fields.length]={
+							name: "LibSub",
+							type: "string",
+							length: "255",
+						};
+						result.metaData.fields[result.metaData.fields.length]={
+							name: "NomPre",
+							type: "string",
+							length: "255",
+						};
+						cb(err,result);
+					});
+				});
+			});
+		});		
 	},
 	// --------------------------------------------------------------------
 	setBaseFact: function(data,cb) {
