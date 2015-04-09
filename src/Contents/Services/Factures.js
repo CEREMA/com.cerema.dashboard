fb9bac51a13cc76b5d3b9a3a6d8ba9996bb247dc
Stéphane Zucatti <stephane.zucatti@cerema.fr>
2015-04-09 13:54:48 +0200

    Update# 726

--- src/Contents/Services/Factures.js
+++ src/Contents/Services/Factures.js
@@ -8,7 +8,7 @@
 Factures = {
 	get: function(o,cb) {
 		var db=Factures.using('db');		
-		//console.log(db.sql('factures_get',{ID: o.id,YEAR: o.year}));
+		console.log(db.sql('factures_get',{ID: o.id,YEAR: o.year}));
 		db.model('dashboard',db.sql('factures_get',{ID: o.id,YEAR: o.year}),cb);
 	},
 	duplicateme: function(tab,ndx,cb) {
