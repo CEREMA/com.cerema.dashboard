aba5036811f0d7ac73f75ad5159ca5973f51ed86
Stéphane Zucatti <stephane.zucati@me.com>
2016-02-09 15:59:24 +0100

    Update# 834

--- src/Contents/Services/Categories.js
+++ src/Contents/Services/Categories.js
@@ -8,10 +8,10 @@
 Categories = {
 	getAll: function(o,cb) {
 		//console.log('SELECT id,libelle FROM categories where YEAR like "%'+o.year+'%"');
-		Categories.using('db').model('dashboard','SELECT id,libelle FROM categories where YEAR like "%'+o.year+'%"',cb);
+		Categories.using('db').model('dashboard','SELECT id,libelle FROM categories where YEAR like "%'+o.year+'%" order by libelle',cb);
 	},
 	getAllYears: function(cb) {
-		Categories.using('db').model('dashboard','SELECT id,libelle FROM categories',cb);
+		Categories.using('db').model('dashboard','SELECT id,libelle FROM categories order by libelle',cb);
 	}
 };
 
