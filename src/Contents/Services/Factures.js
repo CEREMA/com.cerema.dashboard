547c061193e40e4ac8e64c63806881c56c8a4539
Stéphane Zucatti <stephane.zucatti@me.com>
2016-05-18 10:41:33 +0200

    Update# 837

--- src/Contents/Services/Factures.js
+++ src/Contents/Services/Factures.js
@@ -105,19 +105,21 @@ Factures = {
 				Factures.upload_blob(list,ndx+1,cb);
 			} else {
 				Factures.using('db').query('dashboard','insert into docs VALUES ("'+list[ndx].docId+'","-1","-1","-1","-1")',function() {
-					Factures.using('db').post('dashboard','docs',{
-						docId: list[ndx].docId,
-						_blob: App.upload.toBase64(list[ndx].docId),
-						filename: list[ndx].filename,
-						type: list[ndx].filetype,
-						size: list[ndx].filesize
-					},function() {
-						Factures.upload_blob(list,ndx+1,cb);
-					});
+                    App.upload.toBase64(list[ndx].docId,function(err,blob) {
+                        Factures.using('db').post('dashboard','docs',{
+                            docId: list[ndx].docId,
+                            _blob: blob,
+                            filename: list[ndx].filename,
+                            type: list[ndx].filetype,
+                            size: list[ndx].filesize
+                        },function() {
+                            Factures.upload_blob(list,ndx+1,cb);
+                        });                        
+                    });
 				});			
 			}
 		});
-	},	
+	},
 	insert: function(o,cb) {
 		Factures.using('db').post('dashboard','factures',o,function(r){
 			if (o._BLOB) {
